133691a8bf8ac76c20efcf5ada334ef840b5e348
Shaojung <shaojung@gmail.com>
2018-01-27 16:15:56 +0800

    Spinner

--- app/src/main/java/com/example/student/db2018012703/MainActivity.java
+++ app/src/main/java/com/example/student/db2018012703/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.student.db2018012703;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -23,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_dropdown_item_1line, mylist);
         sp.setAdapter(adapter);
 
+    }
+    public void click1(View v)
+    {
+        EditText ed = findViewById(R.id.editText);
+
+        mylist.add(ed.getText().toString());
     }
 }
