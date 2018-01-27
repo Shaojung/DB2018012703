0b55c669012b5b2f2459d2f08d614c38d1746b9b
Shaojung <shaojung@gmail.com>
2018-01-27 15:53:18 +0800

    Spinner

--- app/src/main/java/com/example/student/db2018012703/MainActivity.java
+++ app/src/main/java/com/example/student/db2018012703/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.student.db2018012703;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sp = findViewById(R.id.spinner);
-        
+        String data[] = {"蘋果", "香蕉", "西瓜", "百香果"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, data);
+        sp.setAdapter(adapter);
+
     }
 }
